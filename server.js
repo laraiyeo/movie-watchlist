d908c900e7b0f30dd29da3bd8bbc807f19687d41
Oluwadamilola Laraiyetan <laraiyeo@mcmaster.ca>
2025-03-03 11:18:58 -0500

    Added form to add new movies

--- server.js
+++ server.js
@@ -6,24 +6,24 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Set up Mustache as the template engine
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-// Movie Data (Temporary)
-let movies = [
-    { id: 1, title: "Inception", genre: "Sci-Fi", year: 2010, rating: 8.8, watched: false },
-    { id: 2, title: "The Dark Knight", genre: "Action", year: 2008, rating: 9.0, watched: true }
-];
+let movies = [];
 
-// Routes
 app.get('/', (req, res) => {
     res.render('index', { movies });
+});
+
+app.post('/add', (req, res) => {
+    const { title, genre, year, rating } = req.body;
+    const newMovie = { id: movies.length + 1, title, genre, year, rating, watched: false };
+    movies.push(newMovie);
+    res.redirect('/');
 });
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
